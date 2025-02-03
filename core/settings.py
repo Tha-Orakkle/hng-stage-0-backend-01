90c0f8338d66290d72b7c9b612a0b2b1275db25e
tha_orakkle <adegbiranayinoluwa.paul@yahoo.com>
2025-02-03 18:34:01 +0100

    small fix

--- core/settings.py
+++ core/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-#&z!_!')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False) == 'True'
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', ',127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -44,8 +44,6 @@ INSTALLED_APPS = [
 
     # Local apps
     'api',
-
- 
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
